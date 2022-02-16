bdf97b9ae26ad57327be48ee42ab049309fb1558
rappanuiok <fran_rappa@outlook.com>
2022-02-16 17:23:39 -0300

    add redux and skill archive created

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,78 +1,11 @@
-import React from "react";
 import { Card, Row } from "react-bootstrap";
-import javascript from "../../assets/svg/javascript.svg";
-import html from "../../assets/svg/html.svg";
-import css from "../../assets/svg/css.svg";
-import react from "../../assets/svg/react.svg";
-import jquery from "../../assets/svg/jquery.svg";
-import sass from "../../assets/svg/sass.svg";
-import bootstrap from "../../assets/svg/bootstrap.svg";
-import ajax from "../../assets/svg/ajax.svg";
-import npm from "../../assets/svg/npm.svg";
-import git from "../../assets/svg/git.svg";
-import firebase from "../../assets/svg/firebase.svg";
-import Letter from "../Letter/Letter";
+
 import ItemSkill from "./ItemSkill";
+import Letter from "../Letter/Letter";
+import React from "react";
+import { skills } from "../../utils/data/skills";
 
 function Skills() {
-  const skills = [
-    {
-      title: "Javascript",
-      img: javascript,
-      href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    },
-    {
-      title: "HTML",
-      img: html,
-      href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    },
-    {
-      title: "CSS",
-      img: css,
-      href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    },
-    {
-      title: "React JS",
-      img: react,
-      href: "https://reactjs.org/",
-    },
-    {
-      title: "jQuery",
-      img: jquery,
-      href: "https://jquery.com/",
-    },
-    {
-      title: "Sass",
-      img: sass,
-      href: "https://sass-lang.com/",
-    },
-    {
-      title: "Bootstrap",
-      img: bootstrap,
-      href: "https://getbootstrap.com/",
-    },
-    {
-      title: "AJAX",
-      img: ajax,
-      href: "https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX",
-    },
-    {
-      title: "npm",
-      img: npm,
-      href: "https://www.npmjs.com/",
-    },
-    {
-      title: "Git",
-      img: git,
-      href: "https://git-scm.com/",
-    },
-    {
-      title: "Firebase",
-      img: firebase,
-      href: "https://firebase.google.com/",
-    },
-  ];
-
   return (
     <section
       className="d-flex flex-column justify-content-center align-items-center section__container"
---
+++ src/utils/data/skills.js
@@ -0,0 +1,75 @@
+import ajax from "../../assets/svg/ajax.svg";
+import bootstrap from "../../assets/svg/bootstrap.svg";
+import css from "../../assets/svg/css.svg";
+import firebase from "../../assets/svg/firebase.svg";
+import git from "../../assets/svg/git.svg";
+import html from "../../assets/svg/html.svg";
+import javascript from "../../assets/svg/javascript.svg";
+import jquery from "../../assets/svg/jquery.svg";
+import npm from "../../assets/svg/npm.svg";
+import react from "../../assets/svg/react.svg";
+import redux from "../../assets/svg/redux.svg";
+import sass from "../../assets/svg/sass.svg";
+
+export const skills = [
+  {
+    title: "Javascript",
+    img: javascript,
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    title: "HTML",
+    img: html,
+    href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    title: "CSS",
+    img: css,
+    href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    title: "React JS",
+    img: react,
+    href: "https://reactjs.org/",
+  },
+  {
+    title: "Redux",
+    img: redux,
+    href: "https://redux.js.org/",
+  },
+  {
+    title: "jQuery",
+    img: jquery,
+    href: "https://jquery.com/",
+  },
+  {
+    title: "Sass",
+    img: sass,
+    href: "https://sass-lang.com/",
+  },
+  {
+    title: "Bootstrap",
+    img: bootstrap,
+    href: "https://getbootstrap.com/",
+  },
+  {
+    title: "AJAX",
+    img: ajax,
+    href: "https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX",
+  },
+  {
+    title: "npm",
+    img: npm,
+    href: "https://www.npmjs.com/",
+  },
+  {
+    title: "Git",
+    img: git,
+    href: "https://git-scm.com/",
+  },
+  {
+    title: "Firebase",
+    img: firebase,
+    href: "https://firebase.google.com/",
+  },
+];
