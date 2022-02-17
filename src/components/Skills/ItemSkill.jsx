d7a8125c8057ed5c510cd004efcdcba0f72ed664
rappanuiok <fran_rappa@outlook.com>
2022-02-16 21:19:04 -0300

    skills align-center

--- src/components/Skills/ItemSkill.jsx
+++ src/components/Skills/ItemSkill.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { Col } from "react-bootstrap";
 import "./ItemSkill.css";
+
+import { Col } from "react-bootstrap";
+import React from "react";
 
 function ItemSkill({ href, img, title }) {
   return (
-    <Col xs={4} md={2}>
+    <Col
+      xs={4}
+      md={2}
+      className="d-flex align-items-center justify-content-center"
+    >
       <a href={href} target="_blank" rel="noopener noreferrer" align="center">
         <img src={img} title={title} alt={title} className="img__skill my-2" />
       </a>
