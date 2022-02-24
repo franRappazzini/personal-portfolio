3ebd85a2d4d6ff004a15465db3624a2f243406ad
rappanuiok <fran_rappa@outlook.com>
2022-02-24 10:24:46 -0300

    modularized

--- src/components/Projects/ProjectsContainer.jsx
+++ src/components/Projects/ProjectsContainer.jsx
@@ -3,37 +3,9 @@ import "./Project.css";
 import Letter from "../Letter/Letter";
 import Project from "./Project";
 import React from "react";
-import screenMarilina from "../../assets/img/screenMarilina.png";
-import screenMessage from "../../assets/img/screenMessage.png";
-import screenReact from "../../assets/img/screenReact.png";
+import { projects } from "../../utils/data/projects";
 
 function ProjectsContainer() {
-  const projects = [
-    {
-      img: screenReact,
-      title: "Proyecto final ReactJS",
-      description:
-        "Sitio creado con ReactJS para entrega final del curso en Coderhouse",
-      githubLink:
-        "https://github.com/franRappazzini/Proyecto_Final_React_Coder",
-      link: "https://proyecto-final-react-coder-one.vercel.app/",
-    },
-    {
-      img: screenMessage,
-      title: "JavaScript Message App",
-      description: "Sitio de mensajeria creado con JavaScript, HTML y CSS",
-      githubLink: "https://github.com/franRappazzini/MessagesApp",
-      link: "https://franrappazzini.github.io/MessagesApp/",
-    },
-    {
-      img: screenMarilina,
-      title: "E-commerce para Marilina",
-      description: "Creado con JavaScript, HTML, CSS, Firebase y más",
-      githubLink: "https://github.com/franRappazzini/ecommerce-marilina",
-      link: "https://ropadeportivamf.com.ar/",
-    },
-  ];
-
   return (
     <section>
       <section className="d-flex justify-content-center" data-aos="zoom-in-up">
