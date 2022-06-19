f79eff5f5ab5bc99fa244179cd287b6744aa1f90
Francisco Rappazzini <fran_rappa@outlook.com>
2022-06-19 00:17:45 -0300

    random color in Letters

--- src/components/Letter/Letter.jsx
+++ src/components/Letter/Letter.jsx
@@ -1,9 +1,41 @@
 import "./Letter.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 function Letter({ letter }) {
-  return <span className="letter__titles m-0">{letter}</span>;
+  const [hover, setHover] = useState(false);
+
+  const style = {
+    hover: { color: randomColor() },
+    default: { color: "white" },
+  };
+
+  function handleHover() {
+    setHover(!hover);
+  }
+
+  function randomColor() {
+    const colors = [
+      "#ffbe0b",
+      "#fb5607",
+      "#ff006e",
+      "#8338ec",
+      "#3a86ff",
+      "#e41f1f",
+    ];
+    return colors[Math.floor(Math.random() * 6)];
+  }
+
+  return (
+    <span
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      className="letter__titles m-0"
+      style={hover ? style.hover : style.default}
+    >
+      {letter}
+    </span>
+  );
 }
 
 export default Letter;
