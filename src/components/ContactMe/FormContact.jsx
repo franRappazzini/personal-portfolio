0f7867f640a0c1789fcb5ce8c3e2e658f420de61
franRappazzini <fran_rappa@outlook.com>
2023-09-15 19:30:56 -0300

    feat - emailjs integration

--- src/components/ContactMe/FormContact.jsx
+++ src/components/ContactMe/FormContact.jsx
@@ -1,26 +1,76 @@
 import "./FormContact.css";
 
 import { Col, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
 
 import Button from "@restart/ui/esm/Button";
-import React from "react";
+import CustomToast from "../CustomToast/CustomToast";
+import { send } from "@emailjs/browser";
 import { titleConvert } from "../../utils/functions";
 
+const { REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_PUBLIC_KEY } = process.env;
 const title = "Contact";
 
 function FormContact() {
+  const [form, setForm] = useState({ name: "", email: "", message: "", loading: false });
+  const [toast, setToast] = useState({ error: false, ok: false, message: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!form.name || !form.email || !form.message)
+      return setToast({
+        ...toast,
+        error: true,
+        message:
+          "You must complete all the fields, if you have done so please contact me by another means. Thank you!",
+      });
+
+    setForm({ ...form, loading: true });
+
+    try {
+      const res = await send(
+        REACT_APP_SERVICE_ID,
+        REACT_APP_TEMPLATE_ID,
+        form,
+        REACT_APP_PUBLIC_KEY
+      );
+
+      console.log({ res });
+
+      if (res?.status !== 200) throw new Error(res.text);
+
+      setToast({
+        ...toast,
+        ok: true,
+        message: "Thank you for contacting me, I will respond to you soon!",
+      });
+      setForm({ name: "", email: "", message: "", loading: false });
+    } catch (err) {
+      console.log("send email error:", err);
+      setToast({
+        ...toast,
+        error: true,
+        message: "An unexpected error has occurred, please contact me by another means. Thank you!",
+      });
+      setForm({ ...form, loading: false });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
   return (
-    <section className="d-flex flex-column justify-content-center" style={{ height: "100vh" }}>
+    <section
+      className="d-flex flex-column justify-content-center position-relative"
+      style={{ height: "100vh" }}
+    >
       <section className="d-flex justify-content-center" data-aos="fade-up">
         {titleConvert(title)}
       </section>
 
-      <Form
-        action="https://formsubmit.co/fran_rappa@outlook.com"
-        method="POST"
-        className="mb-3"
-        data-aos="fade-up"
-      >
+      <Form className="mb-3" data-aos="fade-up" onSubmit={handleSubmit}>
         <Row>
           <Col xs={12} md={6}>
             <Form.Group className="mb-3" controlId="name">
@@ -31,6 +81,8 @@ function FormContact() {
                 placeholder="Name"
                 className="form__control"
                 required
+                value={form.name}
+                onChange={handleChange}
               />
             </Form.Group>
           </Col>
@@ -43,6 +95,8 @@ function FormContact() {
                 placeholder="your@email.com"
                 className="form__control"
                 required
+                value={form.email}
+                onChange={handleChange}
               />
             </Form.Group>
           </Col>
@@ -51,18 +105,26 @@ function FormContact() {
               <Form.Label>Message</Form.Label>
               <Form.Control
                 as="textarea"
-                name="textarea"
+                name="message"
                 rows={3}
                 className="form__control"
                 required
+                value={form.message}
+                onChange={handleChange}
               />
             </Form.Group>
-            <Button className="btn btn-outline-light btn__submit" type="submit">
-              Send
+            <Button
+              className="btn btn-outline-light btn__submit"
+              type="submit"
+              disabled={form.loading}
+            >
+              {form.loading ? "Sending..." : "Send"}
             </Button>
           </Col>
         </Row>
       </Form>
+
+      <CustomToast toast={toast} setToast={setToast} />
     </section>
   );
 }
---
+++ src/components/CustomToast/CustomToast.jsx
@@ -0,0 +1,40 @@
+import "./CustomToast.css";
+
+import { Toast, ToastContainer } from "react-bootstrap";
+
+import React from "react";
+
+function CustomToast({ toast, setToast }) {
+  const toggleShow = () => setToast({ ...toast, error: false, ok: false });
+
+  return (
+    <ToastContainer
+      position="top-center"
+      className={`bg-${
+        toast.ok ? "success" : toast.error ? "danger" : "bg-dark"
+      } my-4 toast_container rounded position-fixed`}
+    >
+      <Toast
+        show={toast.ok || toast.error}
+        onClose={toggleShow}
+        className={`bg-${toast.ok ? "success" : toast.error ? "danger" : "bg-dark"} rounded`}
+        delay={6000}
+        autohide
+        animation={false}
+      >
+        <Toast.Header className={`bg-${toast.ok ? "success" : toast.error ? "danger" : "bg-dark"}`}>
+          <strong className="me-auto toast_title">
+            {toast.ok ? "Email sent successfully!" : "Error sending email"}
+          </strong>
+        </Toast.Header>
+        <Toast.Body
+          className={`bg-${toast.ok ? "success" : toast.error ? "danger" : "bg-dark"} toast_text`}
+        >
+          {toast.message}
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+}
+
+export default CustomToast;
