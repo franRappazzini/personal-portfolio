28add518dd3d47e5765fe9ede5f700cb24151ebb
franRappazzini <fran_rappa@outlook.com>
2025-04-18 15:53:08 -0300

    chore: add freelance experience

--- src/utils/data/experience.js
+++ src/utils/data/experience.js
@@ -1,18 +1,26 @@
 export const experience = [
   {
-    i: 1,
+    i: 2,
     role: "Full Stack Blockchain Developer",
     company: "Inside Dark Studio",
     date: "April 2023 - present",
     description:
-      "I developed the main website where I integrated Unity games into React, allowing users to play directly on the web. Additionally, I developed the user database using the Motoko language and created an NFT marketplace. I also implemented the creation of a custom token and integrated ckBTC (Bitcoin on Internet Computer) for trading on the platform.",
+      "I develop various types of applications together with the team, primarily in blockchain. I mainly use Rust for smart contracts and Next.js or React.js with Tailwind for both frontend and backend.",
   },
   {
-    i: 0,
+    i: 1,
     role: "Full Stack Blockchain Developer",
     company: "OPIS",
     date: "October 2022 - April 2023",
     description:
-      "I developed end-to-end solutions using tools such as Hardhat, Next.js, and Node.js for integrating Blockchain with its respective FrontEnd and BackEnd. Additionally, I worked collaboratively with the team to ensure an intuitive user interface and successfully launch the MVP to the market.",
+      "Development of the main website and its integration with smart contracts using Hardhat, Next.js, Node.js, and styling libraries, in collaboration with the team to successfully launch the MVP to the market.",
+  },
+  {
+    i: 0,
+    role: "Full Stack Web Developer",
+    company: "Freelance",
+    date: "August 2021 - August 2021",
+    description:
+      "Development of various websites, including static and dynamic sites, portfolios, and e-commerce solutions for different types of clients. Primarily using Next.js or React.js along with Node.js, styling libraries, and/or Sass for the frontend and backend, as well as SQL with ORM for database management.",
   },
 ];
