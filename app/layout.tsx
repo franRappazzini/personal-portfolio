b0dcd0e97f88af9e1c0a0289bfbe3019aa525b05
franRappazzini <fran_rappa@outlook.com>
2025-06-21 20:04:36 -0300

    feat: favicon + roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import "./globals.css";
 
-import { Geist, Geist_Mono } from "next/font/google";
-
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
   subsets: ["latin"],
 });
 
@@ -29,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${roboto.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
