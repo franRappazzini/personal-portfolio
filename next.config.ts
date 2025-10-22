649658b6631badb2b40e0677e6b22f728fb41537
franRappazzini <fran_rappa@outlook.com>
2025-10-22 11:39:05 -0300

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // comentado porque uso rappazzini.dev.ar como dominio principal
+  // basePath: "/personal-portfolio",
+  // assetPrefix: "/personal-portfolio/",
 };
 
 export default nextConfig;
