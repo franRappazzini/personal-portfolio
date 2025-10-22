a2c89c6b3e754b2f74d30b87bb918355bce61317
franRappazzini <fran_rappa@outlook.com>
2025-10-22 11:55:15 -0300

    Updates

--- lib/env.ts
+++
@@ -1,3 +0,0 @@
-export const EMAILJS_SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string;
-export const EMAILJS_TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string;
-export const EMAILJS_PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string;
