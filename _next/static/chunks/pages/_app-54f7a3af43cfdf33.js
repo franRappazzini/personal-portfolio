d5f405dc0a6b781ac49f8784e365ad404bcf9221
franRappazzini <fran_rappa@outlook.com>
2026-06-02 15:05:39 -0300

    Updates

---
+++ _next/static/chunks/pages/_app-54f7a3af43cfdf33.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6593:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1555)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6593),n(2651))),_N_E=_.O()}]);
